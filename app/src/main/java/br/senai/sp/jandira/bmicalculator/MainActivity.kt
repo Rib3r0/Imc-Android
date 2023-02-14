bed5287b787e412a098f3eeb82f94c806d07996b
EDUARDO RIBEIRO PIMENTA <22282115@dsn127m1242269.cfp127aluno.local>
2023-02-14 09:23:11 -0300

    first commit

---
+++ app/src/main/java/br/senai/sp/jandira/bmicalculator/MainActivity.kt
@@ -0,0 +1,109 @@
+package br.senai.sp.jandira.bmicalculator
+
+import android.graphics.Bitmap
+import android.graphics.drawable.BitmapDrawable
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import br.senai.sp.jandira.bmicalculator.model.Client
+import br.senai.sp.jandira.bmicalculator.model.Product
+import br.senai.sp.jandira.bmicalculator.ui.theme.BMICalculatorTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val p1 = Product()
+        p1.id = 100
+
+        val c1 = Client(100,"Pedro")
+
+
+        setContent {
+            BMICalculatorTheme {
+            CalculatorScreen();
+            }
+        }
+    }
+}
+@Preview(showSystemUi = true)
+@Composable
+fun CalculatorScreen(){
+    Surface(modifier = Modifier.fillMaxSize()) {
+        Column(modifier =  Modifier.fillMaxSize()) {
+            //HEADER
+            Column(
+                modifier =  Modifier.fillMaxWidth(),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Image(
+                    painter = painterResource(R.drawable.bmi),
+                    contentDescription = "",
+                    modifier = Modifier.size(128.dp)
+                )
+                Text(
+                    text = stringResource(id = R.string.title),
+                    fontSize = 32.sp,
+                    color = Color.Blue,
+                    letterSpacing = 4.sp,
+                    fontWeight = FontWeight.Bold
+                )
+            }
+            //FORM
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(24.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(text = stringResource(id = R.string.weight_label), modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(bottom = 10.dp))
+                OutlinedTextField(value = "", onValueChange = {},modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(bottom = 10.dp),
+                    shape = RoundedCornerShape(16.dp)
+                )
+                Text(text = stringResource(id = R.string.height_label), modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(bottom = 10.dp))
+                OutlinedTextField(value = "", onValueChange = {},modifier = Modifier.fillMaxWidth(), shape = RoundedCornerShape(16.dp) )
+                Spacer(modifier = Modifier.height(28.dp))
+                Button(onClick = { /*TODO*/ }, modifier = Modifier.fillMaxWidth(), shape = RoundedCornerShape(16.dp) ) {
+                    Text(text = stringResource(id = R.string.button_calculate ) )
+                }
+            }
+            //FOOTER
+            Column() {
+                Card(
+                    modifier = Modifier
+                        .fillMaxSize(),
+                    backgroundColor = Color(red = 79, green = 54, blue = 232)
+
+                    ) {
+
+                }
+
+            }
+        }
+    }
+}
+
---
+++ app/src/main/java/br/senai/sp/jandira/bmicalculator/model/Client.kt
@@ -0,0 +1,9 @@
+package br.senai.sp.jandira.bmicalculator.model
+
+data class Client(
+    var id: Int,
+    var name: String,
+    var salary: Double = 0.0
+
+
+)
