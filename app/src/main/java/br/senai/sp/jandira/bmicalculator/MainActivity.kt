00b086d209ab1f0f03bb03e3a68e3c777bd18838
Eduardo Ribeiro Pimenta <eduardorpimentaa@gmail.com>
2023-02-14 11:31:01 -0300

    ad

--- app/src/main/java/br/senai/sp/jandira/bmicalculator/MainActivity.kt
+++ app/src/main/java/br/senai/sp/jandira/bmicalculator/MainActivity.kt
@@ -1,59 +1,61 @@
 package br.senai.sp.jandira.bmicalculator
 
-import android.graphics.Bitmap
-import android.graphics.drawable.BitmapDrawable
-import android.icu.text.ListFormatter.Width
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import br.senai.sp.jandira.bmicalculator.model.Client
-import br.senai.sp.jandira.bmicalculator.model.Product
+import br.senai.sp.jandira.bmicalculator.calculate.calculate
 import br.senai.sp.jandira.bmicalculator.ui.theme.BMICalculatorTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        val p1 = Product()
-        p1.id = 100
-
-        val c1 = Client(100,"Pedro")
-
-
         setContent {
             BMICalculatorTheme {
-            CalculatorScreen();
+                CalculatorScreen();
             }
         }
     }
 }
+
 @Preview(showSystemUi = true)
 @Composable
-fun CalculatorScreen(){
+fun CalculatorScreen() {
+
+    var weightState = rememberSaveable() {
+        mutableStateOf("")
+    }
+    var heightState = rememberSaveable() {
+        mutableStateOf("")
+    }
+    var bmiState = rememberSaveable() {
+        mutableStateOf("0.0")
+    }
+
     Surface(modifier = Modifier.fillMaxSize()) {
-        Column(modifier =  Modifier.fillMaxSize()) {
+        Column(modifier = Modifier.fillMaxSize()) {
             //HEADER
             Column(
-                modifier =  Modifier.fillMaxWidth(),
+                modifier = Modifier.fillMaxWidth(),
                 horizontalAlignment = Alignment.CenterHorizontally
             ) {
                 Image(
@@ -76,21 +78,53 @@ fun CalculatorScreen(){
                     .padding(24.dp),
                 horizontalAlignment = Alignment.CenterHorizontally
             ) {
-                Text(text = stringResource(id = R.string.weight_label), modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(bottom = 10.dp))
-                OutlinedTextField(value = "", onValueChange = {},modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(bottom = 10.dp),
-                    shape = RoundedCornerShape(16.dp)
+                Text(
+                    text = stringResource(
+                        id = R.string.weight_label
+                    ),
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(bottom = 10.dp)
                 )
-                Text(text = stringResource(id = R.string.height_label), modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(bottom = 10.dp))
-                OutlinedTextField(value = "", onValueChange = {},modifier = Modifier.fillMaxWidth(), shape = RoundedCornerShape(16.dp) )
-                Spacer(modifier = Modifier.height(28.dp))
-                Button(onClick = { /*TODO*/ }, modifier = Modifier.fillMaxWidth(), shape = RoundedCornerShape(16.dp) ) {
-                    Text(text = stringResource(id = R.string.button_calculate ) )
+                OutlinedTextField(
+                    value = weightState.value,
+                    onValueChange = {
+                        Log.i("ds2m", it)
+                        weightState.value = it
+                    },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(bottom = 10.dp),
+                    shape = RoundedCornerShape(16.dp),
+                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number, autoCorrect = false)
+                )
+                Text(
+                    text = stringResource(id = R.string.height_label), modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(bottom = 10.dp)
+                )
+                OutlinedTextField(
+                    value = heightState.value,
+                    onValueChange = {
+                        Log.i("ds2m", it)
+                        heightState.value = it
+                    },
+                    modifier = Modifier
+                        .fillMaxWidth(),
+                    shape = RoundedCornerShape(16.dp),
+                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number, autoCorrect = false)
+                )
+                Spacer(
+                    modifier = Modifier.height(28.dp)
+                )
+                Button(
+                    onClick = {
+                    bmiState.value = calculate(weight = weightState.value.toDouble(), height = heightState.value.toDouble()).toString()
+                    },
+                    modifier = Modifier.fillMaxWidth(),
+                    shape = RoundedCornerShape(16.dp)
+                ) {
+                    Text(text = stringResource(id = R.string.button_calculate))
                 }
             }
             //FOOTER
@@ -99,10 +133,10 @@ fun CalculatorScreen(){
                     modifier = Modifier
                         .fillMaxSize(),
                     backgroundColor = Color(red = 79, green = 54, blue = 232),
-                    shape = RoundedCornerShape(15.dp)
+                    shape = RoundedCornerShape(topStart = 48.dp, topEnd = 48.dp)
 
 
-                    ) {
+                ) {
                     Column(
                         modifier = Modifier.fillMaxSize(),
                         verticalArrangement = Arrangement.SpaceEvenly,
@@ -114,7 +148,7 @@ fun CalculatorScreen(){
                             fontSize = 32.sp
                         )
                         Text(
-                            text = "0.0",
+                            text = bmiState.value,
                             color = Color.White,
                             fontSize = 48.sp
                         )
@@ -132,7 +166,7 @@ fun CalculatorScreen(){
                             Button(onClick = { /*TODO*/ }) {
                                 Text(text = stringResource(id = R.string.share))
                             }
-                            
+
                         }
                     }
 
--- app/src/main/java/br/senai/sp/jandira/bmicalculator/model/Client.kt
+++
@@ -1,9 +0,0 @@
-package br.senai.sp.jandira.bmicalculator.model
-
-data class Client(
-    var id: Int,
-    var name: String,
-    var salary: Double = 0.0
-
-
-)
